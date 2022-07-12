e2770d23c6ab76519d0e3c6b194f32d61d25108b
tgrvn <94200506+tgrvn@users.noreply.github.com>
2022-07-12 11:37:50 +0300

    template

---
+++ views/comment-form.php
@@ -0,0 +1,9 @@
+<section>
+    <h3>comment</h3>
+    <form method="POST">
+        <input type="text" name="phone" placeholder="+38 0XX XXX-XX-XX" required>
+        <textarea name="comment" placeholder="comment..." cols="30" rows="10" required></textarea>
+
+        <input type="submit" value="send comment">
+    </form>
+</section>
---
+++ views/lucky-ticket-form.php
@@ -0,0 +1,9 @@
+<section>
+    <h3>lucky ticket</h3>
+    <form method="POST">
+        <input type="text" name="min" placeholder="min" required>
+        <input type="text" name="max" placeholder="max" required>
+
+        <input type="submit" name="ticket" value="send ticket">
+    </form>
+</section>
